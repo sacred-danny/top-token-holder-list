a6bb69d3d77bb8aa7a2e59b8a7cdf87b28221709
sacred-danny <sacred.danny.jin@gmail.com>
2023-05-24 15:42:21 +0900

    feat: cheerio

--- index.js
+++ index.js
@@ -6,4 +6,17 @@ const axiosResponse = await axios.request({
   url: "https://ethplorer.io/address/0xdac17f958d2ee523a2206206994597c13d831ec7#chart=candlestick&pageTab=holders&tab=tab-holders&pageSize=100&holders=1",
 });
 
-console.log('axiosResponse: ', axiosResponse.data);
+if (axiosResponse && axiosResponse.data) {
+  const $ = cheerio.load(axiosResponse.data);
+  const addressTds = $('.toggle-inline-controls');
+  for await (const td of addressTds) {
+    const item = $(td);
+    const children = item.children();
+    if (children.length > 0) {
+      const address = $(children[0]).innerText;
+      const innerText = $(children[children.length - 1]).innerText;
+      console.log(`address: ${address}, innerText: ${innerText}`);
+    }
+  }
+}
+
