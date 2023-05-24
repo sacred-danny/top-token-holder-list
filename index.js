4b98dbd04f2cfcb925907d7a7b9126322aa61a83
sacred-danny <sacred.danny.jin@gmail.com>
2023-05-24 15:30:07 +0900

    initial commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const axiosResponse = await axios.request({
   method: "GET",
-  url: "https://ethplorer.io/ru/address/0xdac17f958d2ee523a2206206994597c13d831ec7#chart=candlestick&pageTab=holders&tab=tab-holders&pageSize=100&holders=1",
+  url: "https://ethplorer.io/address/0xdac17f958d2ee523a2206206994597c13d831ec7#chart=candlestick&pageTab=holders&tab=tab-holders&pageSize=100&holders=1",
 });
 
 console.log('axiosResponse: ', axiosResponse);
