2d92d6cec2e80a2505d86d4dfdf7c2527886c6fc
sacred-danny <sacred.danny.jin@gmail.com>
2023-05-24 15:48:03 +0900

    update: url

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const axiosResponse = await axios.request({
 });
 
 if (axiosResponse && axiosResponse.data) {
-  const $ = cheerio.load(axiosResponse.data);
+  const $ = await cheerio.load(axiosResponse.data);
   console.log('$: ', $);
   const addressTds = $('.toggle-inline-controls');
   console.log('addressTds length: ', addressTds.length);
