f2b32bb73925af165eead10dc04546ba5f8d67a3
sacred-danny <sacred.danny.jin@gmail.com>
2023-05-24 15:21:48 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import cheerio from 'cheerio';
+import axios from 'axios';
+
+const axiosResponse = await axios.request({
+  method: "GET",
+  url: "https://ethplorer.io/ru/address/0xdac17f958d2ee523a2206206994597c13d831ec7#chart=candlestick&pageTab=holders&tab=tab-holders&pageSize=100&holders=1",
+});
+
+console.log('axiosResponse: ', axiosResponse);
